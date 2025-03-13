46bf1419c863c65d42cd426919e5b48a1acbbead
Albert Ye <aly@ocf.berkeley.edu>
2025-03-13 14:16:38 -0700

    no more spaces

--- .transpire.py
+++ .transpire.py
@@ -1,4 +1,3 @@
-from tkinter.filedialog import Directory
 from transpire.resources import Deployment, Ingress, Secret, Service
 from transpire.utils import get_image_tag
 
@@ -9,28 +8,25 @@ auto_sync = True
 
 def objects():
     svc = Service(
-        name = "rt",
-        selector = {
-            'app': name,
-        },
-        port_on_pod = 80,
-        port_on_svc = 80,
-        # fill rest later..
+        name="rt",
+        selector=dep.get_selector(),
+        port_on_pod=80,
+        port_on_svc=80,
     )
     dep = Deployment(
-        name = "rt",
-        image = get_image_tag("rt"),
-        ports = [ 80 ],
+        name="rt",
+        image=get_image_tag("rt"),
+        ports=[80],
     )
 
     ing = Ingress.from_svc(
-        svc = svc,
-        host = "//rt.ocf.berkeley.edu",
-        path_prefix = "/",
+        svc=svc,
+        host="rt.ocf.berkeley.edu",
+        path_prefix="/",
     )
     sec = Secret(
-        name = "keycloak-secret",
-        string_data = {
+        name="keycloak-secret",
+        string_data={
             'secret': '',
             'encryption_key': '',
         }
@@ -39,27 +35,27 @@ def objects():
     # set the volumes in the Deployment
     dep.obj.spec.template.spec.volumes = [
         client.V1Volume(
-            name = "secrets",
-            host_path = { "path": "/opt/share/kubernetes/secret/rt", "type": "Directory" }
+            name="secrets",
+            host_path={ "path": "/opt/share/kubernetes/secret/rt", "type": "Directory" }
         ),
     ]
     # also set the container used in deployment
     dep.obj.spec.template.spec.containers[0].name = "rt-nginx"
     dep.obj.spec.template.spec.containers[0].env = [
         client.V1EnvVar(
-            name = "SERVER_NAME",
-            value = "rt.ocf.berkeley.edu",
+            name="SERVER_NAME",
+            value="rt.ocf.berkeley.edu",
         ),
         client.V1EnvVar(
-            name = "CLIENT_SECRET",
-            value_from = client.V1EnvVarSource(
-                secret_key_ref = client.V1SecretKeySelector(key = "secret", name = sec.name)
+            name="CLIENT_SECRET",
+            value_from=client.V1EnvVarSource(
+                secret_key_ref=client.V1SecretKeySelector(key="secret", name=sec.name)
             )
         ),
         client.V1EnvVar(
-            name = "ENCRYPTION_KEY",
-            value_from = client.V1EnvVarSource(
-                secret_key_ref = client.V1SecretKeySelector(key = "encryption_key", name = sec.name)
+            name="ENCRYPTION_KEY",
+            value_from=client.V1EnvVarSource(
+                secret_key_ref=client.V1SecretKeySelector(key="encryption_key", name=sec.name)
             )
         )
     ]
